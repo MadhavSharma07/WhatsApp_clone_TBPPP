7eaf8afbf5c39b2d493903e2eb7f806bf70ed839
AdityaNarayanGupta2004 <adityanarayangupta812@gmail.com>
2025-01-21 16:02:50 +0530

    new_auth

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+const authconfig= {
+  providers: [
+    {
+      domain: "https://calm-baboon-16.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+
+export default authconfig;
