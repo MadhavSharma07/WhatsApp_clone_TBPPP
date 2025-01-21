5b814761caf3c5a7ee13ae08b77eca83c5fb8689
pushpaksinghal <pushpaksinghal1234@example.com>
2025-01-21 16:43:07 +0530

    hi

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+
+const authconfig= {
+    providers: [
+      {
+        domain: "https://calm-baboon-16.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+
+  export default authconfig;
--- src/components/home/chat-placeholder.tsx
+++ src/components/home/chat-placeholder.tsx
@@ -5,13 +5,12 @@ import { Button } from "../ui/button";
 const ChatPlaceHolder = () => {
 
 	const handleButtonClick = () => {
-		// Redirect to the Microsoft Store link
 		window.open("https://apps.microsoft.com/home?hl=en-us&gl=IN", "_blank", "noopener,noreferrer");
 	};
 	return (
 		<div className='w-3/4 bg-gray-secondary flex flex-col items-center justify-center py-10'>
 			<div className='flex flex-col items-center w-full justify-center py-10 gap-4'>
-				<Image src={"/desktop-hero.png"} alt='Hero' width={320} height={188} />
+				<Image src={"/desktop-hero.png"} alt='Hero' width={400} height={250} />
 				<p className='text-3xl font-extralight mt-5 mb-2'>Download WhatsApp for Windows</p>
 				<p className='w-1/2 text-center text-gray-primary text-sm text-muted-foreground'>
 					Make calls, share your screen and get a faster experience when you download the Windows app.
