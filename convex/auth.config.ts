6c79c1cec96dac9c7cbcee86715e0d088b4f96e7
pushpaksinghal <pushpaksinghal1234@example.com>
2025-01-21 16:44:56 +0530

    yo

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,6 +1,5 @@
 
 const authconfig= {
-<<<<<<< HEAD
     providers: [
       {
         domain: "https://calm-baboon-16.clerk.accounts.dev",
@@ -11,16 +10,4 @@ const authconfig= {
 
 
   export default authconfig;
-=======
-  providers: [
-    {
-      domain: "https://calm-baboon-16.clerk.accounts.dev",
-      applicationID: "convex",
-    },
-  ]
-};
 
-
-export default authconfig;
-
->>>>>>> 7d7d84b17723c8c7fb444100ed60e978c3a0ae23
